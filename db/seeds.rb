8dc001dee957e5469ad3ea0c03b0be96023ac222
Evan Ang <alethiologist@gmail.com>
2020-06-29 17:39:15 +0800

    minor tweaks

--- db/seeds.rb
+++ db/seeds.rb
@@ -73,7 +73,7 @@ Lair.create(
 Lair.create(
   name: 'Secure and Private Island Hideaway',
   description: 'This beautiful island comes with a private apartment, a rocket launch site,
-                a nuclear reactor, landing pads and docks for transport and a fully-trained,
+                a nuclear reactor, landing pads and docks, as well as a fully-trained,
                 well-equipped group of staff. SPECTRE members, contact me directly for bookings.'
   address: 'Crab Key, Jamaica',
   price_per_night: 1500,
