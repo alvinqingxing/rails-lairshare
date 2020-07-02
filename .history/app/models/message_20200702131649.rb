893cb8af8e1520625b012eecdb8e946a8baf2e0c
Evan Ang <alethiologist@gmail.com>
2020-07-02 13:17:49 +0800

    created models for conversations and messages

--- .history/app/models/message_20200702131649.rb
+++ .history/app/models/message_20200702131649.rb
@@ -4,6 +4,6 @@ class Message < ApplicationRecord
   validates_presence_of :body, :conversation_id, :user_id
 
   def message_time
-    created_at.strftime("%m/%d/%y at %l:%M %p")
+   created_at.strftime(“%m/%d/%y at %l:%M %p”)
   end
 end
--- db/migrate/20200702044909_create_conversations.rb
+++
@@ -1,8 +0,0 @@
-class CreateConversations < ActiveRecord::Migration[6.0]
-  def change
-    create_table :conversations do |t|      
-      t.integer :host_id
-      t.integer :renter_id
-    end
-  end
-end
