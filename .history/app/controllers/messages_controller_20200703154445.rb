e9dceeb94d6d2c960e87ad93632d55b8c3a35fe8
Evan Ang <alethiologist@gmail.com>
2020-07-03 16:05:59 +0800

    SOMEHOW IT WORKS NOW IM NOT GONNA QUESTION IT

---
+++ .history/app/controllers/messages_controller_20200703154445.rb
@@ -0,0 +1,42 @@
+class MessagesController < ApplicationController
+  before_action do
+    @conversation = Conversation.find(params[:conversation_id])
+  end
+
+  def index
+    @messages = @conversation.messages
+    if @messages.length > 10
+      @over_ten = true
+      @messages = @messages[-10..]
+    end
+    if params[:m]
+      @over_ten = false
+      @messages = @conversation.messages
+    end
+    @messages.each do |message|
+      message.read = true if message.user_id != current_user.id
+      message.save
+    end
+    @message = @conversation.messages.new
+  end
+
+  def new
+    @message = @conversation.messages.new
+  end
+
+  def create
+    @message = @conversation.messages.new(message_params)
+    redirect_to conversation_messages_path(@conversation, anchor: "message-#{@message.id}") if @message.save
+  end
+
+  def delete
+    @message = Message.find(params[:message_id])
+    @message.destroy
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :user_id)
+  end
+end
---
+++ .history/config/routes_20200703155052.rb
@@ -0,0 +1,73 @@
+Rails.application.routes.draw do
+
+  get 'reviews/index'
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'reviews/show'
+  get 'reviews/edit'
+  get 'reviews/update'
+  get 'reviews/destroy'
+  devise_for :users
+  root to: 'pages#home'
+  get    "dashboard", to: "pages#dashboard", as: :dashboard
+  get '/user' => "pages#dashboard", :as => :user_root
+
+  resources :lairs do
+    resources :bookings, only: [:create] do
+    end
+    resources :favourites, only: [:create] do
+    end
+    resources :reviews, only: [:create, :show]
+  end
+
+  resources :bookings, only: [:show] do
+    member do
+      get :accept
+      get :reject
+    end
+  end
+
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
+
+  resources :conversations, only: [:index] do
+    resources :messages, only: [:index, :new, :create]
+  end
+
+  resources :favourites, only: [:destroy]
+  resources :reviews, only: [:destroy]
+
+  resources :messages, only: [:destroy]
+
+end
+
+  # get    "lairs/index",          to: "lairs#index"
+  # get    "lairs/new",      to: "lairs#new",  as: :new_lair
+  # post   "lairs",          to: "lairs#create"
+  # # NB: The `show` route needs to be *after* `new` route.
+  # get    "lairs/:lair_id",      to: "lairs#show", as: :lair
+  # get    "lairs/:lair_id/edit", to: "lairs#edit", as: :edit_lair
+  # patch  "lairs/:lair_id",      to: "lairs#update"
+  # delete "lairs/:lair_id",      to: "lairs#destroy"
+
+# # user---------------------------------------------
+#   get    "users",          to: "users#index" #admin
+#   get    "users/:user_id",      to: "users#show", as: :user
+  # get    "users/new",      to: "users#new",  as: :new_user #devise
+  # post   "users",          to: "users#create" #devise
+  # get    "users/:user_id/edit", to: "users#edit", as: :edit_user #devise
+  # patch  "users/:user_id",      to: "users#update" #devise
+  # delete "users/:user_id",      to: "users#destroy" #devise
+
+  # booking------------------------------------------
+  # get    "bookings",          to: "bookings#index" #see all in dashboard
+  # get    "lairs/:lair_id/bookings/new",      to: "bookings#new",  as: :new_booking
+  # post   "lairs/:lair_id/bookings",          to: "bookings#create"
+  # # NB: The `show` route needs to be *after* `new` route.
+  # get    "bookings/:booking_id",      to: "bookings#show", as: :booking
+  # patch  "bookings/:booking_id/accept",      to: "bookings#accept", as: :booking_accept
+  # patch  "bookings/:booking_id/reject",      to: "bookings#reject", as: :booking_reject
+
+  # # Favourite
+  # post   "lairs/:lair_id/favourites",  to: "favourites#create"
+  # get    "favourite",          to: "favourites#index"
+  # delete "favourites/:id",      to: "favourites#destroy"
