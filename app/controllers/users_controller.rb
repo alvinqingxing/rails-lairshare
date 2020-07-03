05a2480787fc973639175bce5fe3a91900a0b2ae
Alvin Lim <alvinch@hawaii.edu>
2020-07-03 15:19:32 +0800

    User CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,15 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = current_user
+    @user.destroy
+    redirect_to root_path
   end
 
   private
