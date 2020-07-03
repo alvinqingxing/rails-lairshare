1123a2db4fa53738ad67ce2e3b83a0bccc4124e3
Evan Ang <alethiologist@gmail.com>
2020-07-03 15:21:00 +0800

    included turbolinks, but its not working

---
+++ .history/app/controllers/messages_controller_20200703150023.rb
@@ -0,0 +1,36 @@
+class MessagesController < ApplicationController
+  before_action do
+    @conversation = Conversation.find(params[:conversation_id])
+  end
+
+  def index
+    @messages = @conversation.messages
+    if @messages.length > 10
+      @over_ten = true
+      @messages = @messages[-10..]
+    end
+    if params[:m]
+      @over_ten = false
+      @messages = @conversation.messages
+    end
+    @messages.each do |message|
+      message.read = true if message.user_id != current_user.id
+      message.save
+    end
+    @message = @conversation.messages.new
+  end
+
+  def new
+    @message = @conversation.messages.new
+  end
+
+  def create
+    @message = @conversation.messages.new(message_params)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :user_id)
+  end
+end
